b74d1386f077423e921c811637def4a4c8dde0d2
Abby Ferguson <AbbyNicole9793@gmail.com>
2025-08-16 19:55:34 -0400

    Project 9 attempt 1

--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -1,4 +1,4 @@
-const settings = {
+export const settings = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
   submitButtonSelector: ".modal__save-btn",
@@ -7,22 +7,22 @@ const settings = {
   errorClass: "modal__error"
 }
 
-const showInputError = (formElement, inputElement, errorMessage, settings) => {
+export const showInputError = (formElement, inputElement, errorMessage, settings) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
   errorElement.textContent = errorMessage;
   inputElement.classList.add(settings.inputErrorClass);
-  errorElement.classList.add(settings.errorClass) 
+  errorElement.classList.add(settings.errorClass)
 };
 
-const hideInputError = (formElement, inputElement, settings) => {
+export const hideInputError = (formElement, inputElement, settings) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  
+
   errorElement.textContent = "";
   inputElement.classList.remove(settings.inputErrorClass);
   errorElement.classList.remove(settings.errorClass)
 };
 
-const checkInputValidity = (formElement, inputElement) => {
+export const checkInputValidity = (formElement, inputElement) => {
   if (!inputElement.validity.valid) {
     showInputError(formElement, inputElement, inputElement.validationMessage, settings);
   } else {
@@ -30,14 +30,14 @@ const checkInputValidity = (formElement, inputElement) => {
   }
 };
 
-const hasInvalidInput = (inputList) => {
+export const hasInvalidInput = (inputList) => {
   return inputList.some((inputElement) => {
     return !inputElement.validity.valid;
   });
-}; 
+};
 
 
-const toggleButtonState = (inputList, buttonElement, settings) => {
+export const toggleButtonState = (inputList, buttonElement, settings) => {
   if (hasInvalidInput(inputList)) {
     disableButton(buttonElement, settings);
   } else {
@@ -46,18 +46,18 @@ const toggleButtonState = (inputList, buttonElement, settings) => {
   }
 }
 
-const disableButton = (buttonElement, settings) => {
+export const disableButton = (buttonElement, settings) => {
   buttonElement.disabled = true;
   buttonElement.classList.add(settings.inactiveButtonClass)
 }
 
-const resetValidation = (formElement, inputList) => {
+export const resetValidation = (formElement, inputList) => {
   inputList.forEach((inputElement) => {
     hideInputError(formElement, inputElement, settings)
   })
 }
 
-const setEventListeners = (formElement, settings) => {
+export const setEventListeners = (formElement, settings) => {
   const inputList = Array.from(formElement.querySelectorAll(settings.inputSelector));
   const buttonElement = formElement.querySelector(settings.submitButtonSelector);
 
@@ -66,7 +66,7 @@ const setEventListeners = (formElement, settings) => {
   inputList.forEach((inputElement) => {
     inputElement.addEventListener("input", function() {
       checkInputValidity(formElement, inputElement);
-      
+
       toggleButtonState(inputList, buttonElement, settings);
     });
   });
@@ -74,12 +74,10 @@ const setEventListeners = (formElement, settings) => {
 
 
 
-const enableValidation = (settings) => {
+export const enableValidation = (settings) => {
   const formList = document.querySelectorAll(settings.formSelector);
   formList.forEach((formElement) => {
     setEventListeners(formElement, settings);
   })
-  
-}
 
-enableValidation(settings);
+}
---
+++ src/utils/helpers.js
@@ -0,0 +1,8 @@
+export function changeText(e, isLoading, loadingText = "Saving", textContent = "Save") {
+  const submitButton = e.submitter;
+  if (isLoading === true) {
+    submitButton.textContent = loadingText
+  } else {
+    submitButton.textContent = textContent
+  }
+}
